4c4b0b08bf8505a09cc485fec8e3e26f73af04c6
Emirhan Ülgen <emirhanulgen34@gmail.com>
2026-04-09 15:24:22 +0100

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.kt
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun isValidBST(root: TreeNode?): Boolean {
+        val array = ArrayList<Int?>()
+
+        traverseInOrder(root, array)
+        for(i in 1 until array.size){
+
+            if(array[i]!! <= array[i - 1]!!){
+                return false
+            }
+            
+        }
+
+        return true
+    }
+
+    fun traverseInOrder(node: TreeNode?, array: ArrayList<Int?>){
+
+        if(node == null){
+            return
+        }
+
+        traverseInOrder(node.left, array)
+        array.add(node.`val`)
+        traverseInOrder(node.right, array)
+
+    }
+}
