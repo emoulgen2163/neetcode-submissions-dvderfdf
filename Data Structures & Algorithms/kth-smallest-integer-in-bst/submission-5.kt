34af3c4cae1eddadb4692fc7c609619db5aafbdd
Emirhan Ülgen <emirhanulgen34@gmail.com>
2026-04-10 15:02:02 +0100

    Add: kth-smallest-integer-in-bst - submission-5

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-5.kt
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun kthSmallest(root: TreeNode?, k: Int): Int {
+        val array = ArrayList<Int?>()
+
+        traversalInOrder(array, root)
+
+        var count = 0
+        while(count != k - 1){
+            count++
+        }
+
+        return array[count] ?: 0
+    }
+
+    fun traversalInOrder(array: ArrayList<Int?>, node: TreeNode?){
+        if(node == null) return
+
+        traversalInOrder(array, node.left)
+        array.add(node.`val`)
+        traversalInOrder(array, node.right)
+    }
+}
