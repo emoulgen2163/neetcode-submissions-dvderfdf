9a1fed5663c64e3207a23fc334e58b132e6b8bb1
Emirhan Ülgen <emirhanulgen34@gmail.com>
2026-04-19 12:54:05 +0100

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.kt
@@ -0,0 +1,37 @@
+class Solution {
+    fun exist(board: Array<CharArray>, word: String): Boolean {     
+        for(i in 0 until board.size){
+            for (j in 0 until board[0].size) {
+                if(backtrack(board, word, i, j, 0)){
+                    return true
+                }
+            }
+        }
+
+        return false
+    }
+
+    fun backtrack(board: Array<CharArray>, word: String, row: Int, col: Int, index: Int): Boolean{
+        if(index == word.length){
+            return true
+        }
+        val outOfBounds = row < 0 || row >= board.size || col < 0 || col >= board[0].size
+
+        if(outOfBounds || word[index] != board[row][col]){
+            return false
+        }
+
+        val c = board[row][col]
+        board[row][col] = '#'
+        
+
+        val result = backtrack(board, word, row - 1, col, index + 1) ||
+            backtrack(board, word, row + 1, col, index + 1) ||
+            backtrack(board, word, row, col - 1, index + 1) ||
+            backtrack(board, word, row, col + 1, index + 1)
+        
+        board[row][col] = c
+
+        return result
+    }
+}
