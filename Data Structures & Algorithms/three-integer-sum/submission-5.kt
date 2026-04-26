2dbe3fecc159f35abd99a2e821d14f8d28071a14
Emirhan Ülgen <emirhanulgen34@gmail.com>
2026-04-26 12:11:47 +0100

    Add: three-integer-sum - submission-5

---
+++ Data Structures & Algorithms/three-integer-sum/submission-5.kt
@@ -0,0 +1,32 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val result = ArrayList<List<Int>>()
+
+        for(i in nums.indices){
+
+            if(i > 0 && nums[i] == nums[i - 1]) continue
+            
+            var left = i + 1
+            var right = nums.size - 1
+            val target = -1 * (nums[i])
+
+            while(left < right){
+                if(nums[left] + nums[right] == target){
+                    result.add(listOf(nums[left], nums[right], nums[i]))
+                    left++
+                    right--     
+
+                    while(left < right && nums[left] == nums[left - 1]) left++
+
+                } else if(nums[left] + nums[right] < target){
+                    left++
+                } else{
+                    right--
+                }
+            }
+        }
+
+        return result
+    }
+}
