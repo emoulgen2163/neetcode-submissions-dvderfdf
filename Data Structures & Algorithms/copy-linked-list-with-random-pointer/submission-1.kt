78415c4bd3e50e341affe7e7476dd134c50f8e46
Emirhan Ülgen <emirhanulgen34@gmail.com>
2026-06-11 14:22:46 +0100

    Add: copy-linked-list-with-random-pointer - submission-1

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-1.kt
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node(var `val`: Int) {
+    var next: Node? = null
+    var random: Node? = null
+}
+*/
+
+class Solution {
+    fun copyRandomList(head: Node?): Node? {
+        if(head == null) return null
+
+        var current = head
+
+        while(current != null){
+            val newNode = Node(current.`val`)
+            newNode?.next = current.next
+            current.next = newNode
+            current = newNode?.next
+        }
+
+        current = head
+        while(current != null){
+            if(current.random != null){
+                current.next?.random = current?.random?.next
+            }
+
+            current = current.next?.next
+        }
+
+        current = head
+        val newHead = head.next
+        var newCurrent = newHead
+
+        while(current != null){
+            current.next = newCurrent?.next
+            current = current.next
+
+            if(current != null){
+                newCurrent?.next = current.next
+                newCurrent = newCurrent?.next
+            }
+        }
+
+        return newHead
+    }
+}
